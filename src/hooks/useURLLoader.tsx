91e47dae3eaf3a30e4590f61a841099c827bef36
tyler4400 <tyler4400@foxmail.com>
2020-04-18 20:42:43 +0800

    fix:http://coding.imooc.com/learn/questiondetail/180913.html

--- src/hooks/useURLLoader.tsx
+++ src/hooks/useURLLoader.tsx
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
-import axios, {AxiosResponse} from "axios";
+import axios from "axios";
 
-const useURLLoader = function <T>(url: string, deps: any[] = []): [T, boolean]{
+const useURLLoader = <T extends any>(url: string, deps: any[] = []): [T, boolean] => {
 	// @ts-ignore
 	const [data, setData] = useState<T>(null);
 	const [loading, setLoading] = useState(false);
